575d27fd590da46f3c022bc0054cd0001a0c1a42
rbeauchemin3 <rbeauchemin3@gatech.edu>
2021-09-26 21:50:01 -0400

    overhauled the base version, removed py2 distinction

--- interactive_lockpicking_game.py
+++ interactive_lockpicking_game.py
@@ -5,14 +5,14 @@ from IPython.core.pylabtools import figsize, getfigs
 from pylab import *
 from numpy import *
 import sys
-import cStringIO
+from io import BytesIO as cStringIO
 from PIL import Image
 np = numpy
 plt = pyplot
 ioff()
 
-print 'You come across a dark cellar room and spot a chest with a lock in the corner'
-yorn = raw_input('Will you attempt to pick the lock? [y]/n: ') or 'y'
+print('You come across a dark cellar room and spot a chest with a lock in the corner')
+yorn = input('Will you attempt to pick the lock? [y]/n: ') or 'y'
 if yorn == 'y':
     x_previous=[]
     y_previous=[]
@@ -23,7 +23,7 @@ if yorn == 'y':
         theta = np.arctan((y-y0)/(x-x0))
         xnew = x0 + (r0 * np.cos(theta))
         ynew = y0 + (r0 * np.sin(theta))
-        if x > x0:    
+        if x > x0:
             plt.plot([x0,xnew],[y0,ynew],'k')
             on_click.xprev = xnew
             on_click.yprev = ynew
@@ -35,15 +35,15 @@ if yorn == 'y':
             on_click.guess = np.degrees(theta+np.pi)
         fig.canvas.draw()
     correct_guess = randint(-90,270)
-    print 'You know that if you can apply leverage at the correct angle, the lock will free.'
+    print('You know that if you can apply leverage at the correct angle, the lock will free.')
     lockpicks = 5
-    print 'You have {0} lockpicks, and one will break if you force it too hard in the wrong direction.'.format(lockpicks)
-    damage = 0
+    print('You have {0} lockpicks, and one will break if you force it too hard in the wrong direction.'.format(lockpicks))
+    pick_strain = 0
     guess = 0
     finished = 'n'
     x0, y0 = 297, 223.5
     while lockpicks != 0 and finished == 'n':
-        print 'Choose an angle with the mouse'
+        print('Choose an angle with the mouse')
         fig, ax = plt.subplots()
         ax.set_xlim(0, 600)
         ax.set_ylim(400,0)
@@ -58,9 +58,9 @@ if yorn == 'y':
         guess = on_click.guess
         x_previous.append(on_click.xprev)
         y_previous.append(on_click.yprev)
-        print guess
-        force = int(raw_input('Choose the force applied (1-5): '))
-        
+        print(guess)
+        force = int(input('Choose the force applied (1-5): '))
+
         if abs(correct_guess-guess) < 3:
             angle = 'correct'
         elif abs(correct_guess-guess) < 15 and abs(correct_guess-guess) >= 3:
@@ -71,11 +71,11 @@ if yorn == 'y':
             angle = 'far'
         else:
             angle = 'realfar'
-            
+
         if angle == 'correct':
             if force == 5:
-                print '*Click*'
-                print 'The lock falls to the floor and the chest opens to reveal gold strewn with shining sapphires and rubies.'
+                print('*Click*')
+                print('The lock falls to the floor and the chest opens to reveal gold strewn with shining sapphires and rubies.')
                 fig, ax = plt.subplots()
                 treasure = Image.open('treasure.png')
                 ax.imshow(treasure)
@@ -83,63 +83,63 @@ if yorn == 'y':
                 finished = 'y'
                 show()
             elif force < 5:
-                print 'The pick is pushed with no resistance.'
-                
+                print('The pick is pushed with no resistance.')
+
         elif angle == 'close':
             if force == 5:
                 lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
+                print('The pick breaks. You now have {0} lockpicks.'.format(lockpicks))
+                pick_strain = 0
             elif force == 4:
-                damage += 1
-                if damage == 3:
+                pick_strain += 1
+                if pick_strain == 3:
                     lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
+                    print('The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks))
                 else:
-                    print 'The pick feels resistance.'
+                    print('The pick feels resistance.')
             else:
-                print 'The pick is pushed with no resistance.'
+                print('The pick is pushed with no resistance.')
         elif angle == 'nearlyclose':
             if force >= 4:
                 lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
+                print('The pick breaks. You now have {0} lockpicks.'.format(lockpicks))
+                pick_strain = 0
             elif force == 3:
-                damage += 1
-                if damage == 3:
+                pick_strain += 1
+                if pick_strain == 3:
                     lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
+                    print('The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks))
                 else:
-                    print 'The pick feels resistance.'
+                    print('The pick feels resistance.')
             else:
-                print 'The pick is pushed with no resistance.'
-                
+                print('The pick is pushed with no resistance.')
+
         elif angle == 'far':
             if force >= 3:
                 lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
+                print('The pick breaks. You now have {0} lockpicks.'.format(lockpicks))
+                pick_strain = 0
             elif force == 2:
-                damage += 1
-                if damage == 3:
+                pick_strain += 1
+                if pick_strain == 3:
                     lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
+                    print('The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks))
                 else:
-                    print 'The pick feels resistance.'
+                    print('The pick feels resistance.')
             else:
-                print 'The pick is pushed with no resistance.'
-                    
+                print('The pick is pushed with no resistance.')
+
         elif angle == 'realfar':
             if force >= 2:
                 lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
+                print('The pick breaks. You now have {0} lockpicks.'.format(lockpicks))
+                pick_strain = 0
             else:
-                damage += 1
-                if damage == 3:
+                pick_strain += 1
+                if pick_strain == 3:
                     lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
+                    print('The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks))
                 else:
-                    print 'The pick feels resistance.'
+                    print('The pick feels resistance.')
     if lockpicks == 0:
-        print 'You\'re all out of picks. You\'ll have to leave this treasure for a more skilled treasure hunter.'
+        print('You\'re all out of picks. You\'ll have to leave this treasure for a more skilled treasure hunter.')
--- lockpicking_game.py
+++ lockpicking_game.py
@@ -1,99 +1,125 @@
-from random import randint
+import random
 
-print 'You come across a dark cellar room and spot a chest with a lock in the corner'
-yorn = raw_input('Will you attempt to pick the lock? [y]/n: ') or 'y'
-if yorn == 'y':
-    correct_guess = randint(-180,180)
-    print 'You bash the key hole and are able to turn it 180 degrees to the left and right.'
-    print 'You know that if you can apply leverage at the correct degree, the lock will free.'
-    lockpicks = 5
-    print 'You have {0} lockpicks, and one will break if you force it too hard into the wrong angle.'.format(lockpicks)
-    damage = 0
-    guess = 0
-    finished = 'n'
-    while lockpicks != 0 and finished == 'n':
-        guess = raw_input('Choose an angle (integer between -180 and 180) [previous]: ') or guess
-        force = int(raw_input('Choose the force applied (1-5): '))
-        guess = int(guess)
-        
-        if abs(correct_guess-guess) < 3:
-            angle = 'correct'
-        elif abs(correct_guess-guess) < 15 and abs(correct_guess-guess) >= 3:
-            angle = 'close'
-        elif abs(correct_guess-guess) < 30 and abs(correct_guess-guess) >= 15:
-            angle = 'nearlyclose'
-        elif abs(correct_guess-guess) < 60 and abs(correct_guess-guess) >= 30:
-            angle = 'far'
+
+class Adventurer:
+    def __init__(self):
+        self.inventory = {'gold': random.randint(20,40),
+                          'picks': random.randint(3,6),
+                          'sapphires': 0,
+                          'rubies': 0}
+
+    def get_inv_count(self, item):
+        return self.inventory[item] if item in self.inventory else 0
+    
+    def alter_inv_count(self, item, alteration):
+        if item not in self.inventory:
+            self.inventory[item] = 0
+        if self.get_inv_count(item) + alteration >= 0:
+            self.inventory[item] = self.get_inv_count(item) + alteration
+        return self.inventory[item] 
+
+    def set_inv_count(self, item, num):
+        self.inventory[item] = num
+
+
+class LockPicking(Adventurer):
+    def __init__(self):
+        super().__init__()
+        self.pick_strain = 0
+        self.picks = super().get_inv_count('picks')
+    
+    def _pick_breaks(self, from_repeat=False):
+        self.picks -= 1
+        self.pick_strain = 0
+        if from_repeat:
+            print('The pick feels resistance and snaps from repeated stress. You now have {0} picks.'.format(self.picks))
         else:
-            angle = 'realfar'
-            
-        if angle == 'correct':
-            if force == 5:
-                print '*Click*'
-                print 'The lock falls to the floor and the chest opens to reveal gold strewn with shining sapphires and rubies.'
-                finished = 'y'
-            elif force < 5:
-                print 'The pick is pushed with no resistance.'
-                
-        elif angle == 'close':
-            if force == 5:
-                lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
-            elif force == 4:
-                damage += 1
-                if damage == 3:
-                    lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
-                else:
-                    print 'The pick feels resistance.'
-            else:
-                print 'The pick is pushed with no resistance.'
-        elif angle == 'nearlyclose':
-            if force >= 4:
-                lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
-            elif force == 3:
-                damage += 1
-                if damage == 3:
-                    lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
-                else:
-                    print 'The pick feels resistance.'
-            else:
-                print 'The pick is pushed with no resistance.'
-                
-        elif angle == 'far':
-            if force >= 3:
-                lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
-            elif force == 2:
-                damage += 1
-                if damage == 3:
-                    lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
-                else:
-                    print 'The pick feels resistance.'
-            else:
-                print 'The pick is pushed with no resistance.'
-                    
-        elif angle == 'realfar':
-            if force >= 2:
-                lockpicks -= 1
-                print 'The pick breaks. You now have {0} lockpicks.'.format(lockpicks)
-                damage = 0
-            else:
-                damage += 1
-                if damage == 3:
-                    lockpicks -= 1
-                    print 'The pick feels resistance and snaps from repeated stress. You now have {0} lockpicks.'.format(lockpicks)
-                else:
-                    print 'The pick feels resistance.'
-    if lockpicks == 0:
-        print 'You\'re all out of picks. You\'ll have to leave this treasure for a more skilled treasure hunter.'
+            print('The pick breaks. You now have {0} picks.'.format(self.picks))
 
+    def lockpicking_sequence(self):
+        print('You come across a dark cellar room and spot a chest with a lock in the corner.')
+        print('You know that if you can apply leverage at the correct degree, the lock will free.')
+        print('You have {0} picks, and one will break if you force it too hard at the wrong angle.'.format(self.picks))
+        yorn = input('Will you attempt to pick the lock? [y]/n: ') or 'y'
+        if yorn.lower()[0] == 'n':
+            print('Not this time... You decide to continue on.')
+        if yorn.lower()[0] == 'y':
+            correct_guess = random.randint(-180,180)
+            self.pick_strain = 0
+            guess = 0
+            while self.picks != 0:
+                guess = input('Choose an angle (integer between -180 and 180) [previous]: ') or guess
+                force = int(input('Choose the force applied (1-5): '))
+                guess = int(guess)
+                
+                if abs(correct_guess-guess) < 3:
+                    angle = 'correct'
+                elif abs(correct_guess-guess) < 15 and abs(correct_guess-guess) >= 3:
+                    angle = 'close'
+                elif abs(correct_guess-guess) < 30 and abs(correct_guess-guess) >= 15:
+                    angle = 'nearlyclose'
+                elif abs(correct_guess-guess) < 60 and abs(correct_guess-guess) >= 30:
+                    angle = 'far'
+                else:
+                    angle = 'realfar'
+                angle = 'correct'
                     
+                if angle == 'correct':
+                    if force == 5:
+                        print('*Click*')
+                        print('The lock falls to the floor and the chest opens to reveal gold strewn with shining sapphires and rubies.')
+                        super().alter_inv_count('gold', random.randint(10,20))
+                        super().alter_inv_count('sapphires', random.randint(1,2))
+                        super().alter_inv_count('rubies', random.randint(1,2))
+                        break
+                    elif force < 5:
+                        print('The pick is pushed with no resistance.')
+                        
+                elif angle == 'close':
+                    if force == 5:
+                        self._pick_breaks()
+                    elif force == 4:
+                        self.pick_strain += 1
+                        if self.pick_strain == 3:
+                            self._pick_breaks(from_repeat=True)
+                        else:
+                            print('The pick feels resistance.')
+                    else:
+                        print('The pick is pushed with no resistance.')
+                elif angle == 'nearlyclose':
+                    if force >= 4:
+                        self._pick_breaks()
+                    elif force == 3:
+                        self.pick_strain += 1
+                        if self.pick_strain == 3:
+                            self._pick_breaks(from_repeat=True)
+                        else:
+                            print('The pick feels resistance.')
+                    else:
+                        print('The pick is pushed with no resistance.')
+                        
+                elif angle == 'far':
+                    if force >= 3:
+                        self._pick_breaks()
+                    elif force == 2:
+                        self.pick_strain += 1
+                        if self.pick_strain == 3:
+                            self._pick_breaks(from_repeat=True)
+                        else:
+                            print('The pick feels resistance.')
+                    else:
+                        print('The pick is pushed with no resistance.')
+                            
+                elif angle == 'realfar':
+                    if force >= 2:
+                        self._pick_breaks()
+                    else:
+                        self.pick_strain += 1
+                        if self.pick_strain == 3:
+                            self._pick_breaks(from_repeat=True)
+                        else:
+                            print('The pick feels resistance.')
+        return
                     
-                    
+if __name__ == '__main__':
+    LockPicking().lockpicking_sequence()
